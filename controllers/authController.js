3b534ad21674a48ff8ef98a83ef9f7024c284bd7
Varun Hegde <varunhegde2k@gmail.com>
2021-09-25 20:40:04 +0530

    changed file structure

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,3 @@
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
-
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const signToken = require('../utils/jwtTokens');
@@ -56,60 +53,8 @@ const login = catchAsync(async (req, res, next) => {
 });
 
 // Protect route middleware
-const protect = catchAsync(async (req, res, next) => {
-	// 1) Getting token and check if it's there
-	// 2) Verification token
-	// 3) Check if user still exists?
-	// 4) Check if user changed password after JWT was issued
-
-	// 1)
-	let token;
-	if (
-		req.headers.authorization &&
-		req.headers.authorization.startsWith('Bearer')
-	) {
-		token = req.headers.authorization.split(' ')[1];
-	}
-
-	if (!token) {
-		return next(
-			new AppError(
-				'You are not logged in! Please log in to continue',
-				401,
-			),
-		);
-	}
-
-	// 2)
-	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-	// 3)
-	const currentUser = await User.findById(decoded.id);
-	if (!currentUser) {
-		return next(
-			new AppError(
-				'The user belonging to this token does no longer exist.',
-				401,
-			),
-		);
-	}
-
-	// 4)
-	if (currentUser.changedPasswordAfter(decoded.iat)) {
-		return next(
-			new AppError(
-				'User recently changed password! Please log in again.',
-				401,
-			),
-		);
-	}
-
-	req.user = currentUser;
-	next();
-});
 
 module.exports = {
 	signup,
 	login,
-	protect,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/authController');
+const authMiddlware = require('../middleware/authMiddleware');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/get-user', authController.protect, async (req, res) => {
+router.get('/get-user', authMiddlware.protect, async (req, res) => {
 	res.json(req.user);
 });
 
